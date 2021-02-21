7c364e3519cc21d440385690b1b3bd0b12e33fe0
Mastak <farkhat.bakiev@mail.ru>
2021-02-21 19:52:41 +0300

    js fix

--- app/js/main.js
+++ app/js/main.js
@@ -1,24 +1,25 @@
-const navOpen = () => {
-   const burger = document.querySelector('.burger');
-   const navRight = document.querySelector('.header-nav-right');
-
-   burger.addEventListener('click', () => {
-      navRight.classList.toggle('navLinks-active');
-      burger.classList.toggle('burger-right');
-   });
-}
-
-function parallaxEffect (event) {
-   const parallax = this.querySelectorAll('.parallax');
-
-   [...parallax].forEach((item) => {
-      const speed = item.dataset.speed;
-      const formula = event.clientX*speed/1000;
-      item.style.transform = `translateX(${formula}px)`;
-      // item.style.transform = `translateY(${formula/4}px)`;
-   });
-}
-
-document.querySelector('.header').addEventListener('mousemove', parallaxEffect);
-
-navOpen();
+document.addEventListener("DOMContentLoaded", () => {
+   const navOpen = () => {
+      const burger = document.querySelector('.burger');
+      const navRight = document.querySelector('.header-nav-right');
+   
+      burger.addEventListener('click', () => {
+         navRight.classList.toggle('navLinks-active');
+         burger.classList.toggle('burger-right');
+      });
+   }
+   
+   function parallaxEffect (event) {
+      const parallax = this.querySelectorAll('.parallax');
+   
+      [...parallax].forEach((item) => {
+         const speed = item.dataset.speed;
+         const formula = event.clientX*speed/1000;
+         item.style.transform = `translateX(${formula}px)`;
+      });
+   }
+   
+   document.querySelector('.header').addEventListener('mousemove', parallaxEffect);
+   
+   navOpen();
+});
