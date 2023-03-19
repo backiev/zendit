175fcfa0debaafb81505bb2ee6b971d42f8f26ea
backiev <farkhat.bakiev@mail.ru>
2023-03-20 00:31:11 +0300

    gulpfile -> ecmascript 6

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,25 @@
-const
-    { src, dest, parallel, series, watch } = require('gulp'),
-    browserSync = require('browser-sync').create(),
-    concat      = require('gulp-concat'),
-    uglify      = require('gulp-uglify-es').default,
-    sass        = require('gulp-sass'),
-    autoprefix  = require('gulp-autoprefixer'),
-    cleancss    = require('gulp-clean-css'),
-    imagemin    = require('gulp-imagemin'),
-    newer       = require('gulp-newer'),
-    del         = require('del'),
-    csso        = require('gulp-csso');
-// npm i browser-sync gulp-concat gulp-uglify-es gulp-sass del gulp-newer gulp-imagemin gulp-clean-css gulp-autoprefixer
+import pkd from 'gulp';
+import imagemin from 'gulp-imagemin';
+import browserSync from 'browser-sync';
+import concat from 'gulp-concat';
+import autoprefix from 'gulp-autoprefixer';
+
+import cleancss from 'gulp-clean-css';
+
+import dartSass from 'sass';
+import gulpSass from 'gulp-sass';
+const sass = gulpSass(dartSass);
+import newer from 'gulp-newer';
+import {deleteAsync} from 'del';
+import csso from 'gulp-csso';
+
+
+
+
+
+
+
+const {src, dest, parallel, series, watch} = pkd;
 
 const browsersync = () => {
     browserSync.init({
@@ -19,14 +28,6 @@ const browsersync = () => {
         online: true
     })
 }
-
-// const libsJS = () => {
-// 	return src([
-// 		])
-// 		.pipe(concat('libs.min.js'))
-// 		// .pipe(uglify())
-// 		.pipe(dest('app/js/'))
-// }
 
 const libsCSS = () => {
 	return src([
@@ -38,12 +39,10 @@ const libsCSS = () => {
 		   overrideBrowserslist: ['Last 10 versions'],
 		   grid: true
      	}))
-     	// .pipe(sass({includePaths: require('node-normalize-scss').includePaths)})
      	.pipe(cleancss({
          level: {
              1: { specialComments: 0}
          },
-         // format: 'beautify'
      	}))
 		.pipe(dest('app/css/'))
 }
@@ -53,7 +52,6 @@ const scripts = () => {
 		'app/js/main.js'
     ])
         .pipe(concat('main.min.js'))
-        .pipe(uglify())
         .pipe(dest('app/js/'))
         .pipe(browserSync.stream())
 }
@@ -68,12 +66,10 @@ const styles = () => {
             overrideBrowserslist: ['Last 10 versions'],
             grid: true
         }))
-        // .pipe(sass({includePaths: require('node-normalize-scss').includePaths)})
         .pipe(cleancss({
             level: {
                 1: { specialComments: 0}
             },
-            // format: 'beautify'
         }))
         .pipe(csso({
         	debug: true
@@ -91,43 +87,23 @@ const images = () => {
 }
 
 const cleanimg = () => {
-    return del('app/img/dest/**/*', { force: true })
+    return deleteAsync('app/img/dest/**/*', { force: true })
 }
 const cleandist = () => {
-    return del('app/dist/**/*', { force: true })
+    return deleteAsync('app/dist/**/*', { force: true })
 }
 
-const buildcopy = async () => {
-    // return src([
-    //     'app/css/**/*.min.css',
-    //     'app/js/**/*.min.js',
-    //     'app/img/dest/**/*',
-    //     'app/**/*.html',
-    // ])
-    //     .pipe(dest('dist'))
-    const 
-    		buildCSS 	= src('app/css/**/*.min.css').pipe(dest('dist/css')),
-		   buildJS 		= src('app/js/**/*.min.js').pipe(dest('dist/js')),
-		   buildIMG 	= src('app/img/**/*').pipe(dest('dist/img')),
-		   buildHTML	= src('app/**/*.html').pipe(dest('dist/'));
-}
 
 const startWatch = () => {
     watch(['app/**/*.sass'], styles);
     watch(['app/**/*.js', '!app/**/*.min.js'], scripts);
     watch('app/**/*.html').on('change', browserSync.reload);
     watch('app/img/src/**/*', images);
-    // watch(['app/**/*.js', '!app/**/libs.min.js'], libsJS)
 }
 
-exports.browsersync = browsersync;
-exports.scripts     = scripts;
-exports.styles      = styles;
-exports.images      = images;
-exports.cleanimg    = cleanimg;
-// exports.libsJS 	  = libsJS;
-exports.libsCSS     = libsCSS;
+export {browsersync, scripts, styles, images, cleanimg, libsCSS};
 
-exports.build       = series(cleandist, styles, scripts, images, buildcopy)
 
-exports.default     = parallel(styles, scripts, browsersync, startWatch);
+export const build = series(cleandist, styles, images, scripts, buildcopy)
+
+export default parallel(styles, scripts, browsersync, startWatch);
